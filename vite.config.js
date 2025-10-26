fee3e2e06549d9e1b2657db44232dee9d089c012
morriskaria <morriskaria542@gmail.com>
2025-10-26 18:33:02 +0300

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-<<<<<<< HEAD
+
   plugins: [react(), tailwindcss()],
 });
-=======
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js' // Remove this line if it exists
-  },
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
-})
->>>>>>> 821e163 (Property listings page)
+
+
+
